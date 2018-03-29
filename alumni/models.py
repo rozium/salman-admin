99a6426e68352c652e19c33f1719e0aafc38d25e
Achmad Fahrurrozi M <fahrurrozi31@gmail.com>
2018-03-29 23:12:10 +0700

    add alumni apps

---
+++ alumni/models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db.models import (
+    Model,
+    CharField,
+    EmailField,
+    TextField
+)
+
+# Create your models here.
+class User(Model):
+    nama = CharField(max_length=50, null=True)
+    email = EmailField(max_length=30, null=True)
+    gender = CharField(max_length=20, null=True)
+    negara = CharField(max_length=20, null=True)
+    kota = CharField(max_length=20, null=True)
+    no_hp = CharField(max_length=12, null=True)
+    univ = CharField(max_length=50, null=True)
+    jurusan = CharField(max_length=50, null=True)
+    ang_kuliah = CharField(max_length=5, null=True)
+    ang_LMD = CharField(max_length=5, null=True)
+    pekerjaan = TextField(null=True)
+    instansi = CharField(max_length=30, null=True)
+    aktifitas = TextField(null=True)
+    tahun_aktif = CharField(max_length=20, null=True)
---
+++ alumni/views.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponsePermanentRedirect
+from django.core.serializers import serialize
+from django.core.serializers.json import DjangoJSONEncoder
+from .models import User
+
+def index(request):
+    return redirect("/login/")
+
+def home(request):
+    return render(request, 'home.html', {})
+
+def verifikasi(request):
+    users = User.objects.all()
+    context = {'users': users, 'usersJSON': serialize('json', User.objects.all(), cls=LazyEncoder)}
+    return render(request, 'verifikasi.html', context)
+
+class LazyEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, YourCustomType):
+            return str(obj)
+        return super().default(obj)
