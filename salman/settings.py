7bceef00ce5164c8d0d90c3292ebfd40c51e09a5
rozium <fahrurrozi31@gmail.com>
2018-04-17 11:37:11 +0700

    migrate to mysql

--- salman/settings.py
+++ salman/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'salman.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'alumni',
+        'USER': 'root',
+        'PASSWORD': 'ppl2018',
     }
 }
 
