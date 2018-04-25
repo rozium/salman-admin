0cd6a8b47f96d68f31dcc01343d39f097dbd7285
Achmad Fahrurrozi M <fahrurrozi31@gmail.com>
2018-04-25 20:56:08 +0700

    fathur

--- alumni/tests.py
+++ alumni/tests.py
@@ -2,5 +2,27 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User,ArticleClip,About
 
 # Create your tests here.
+class AboutTestCase(TestCase):
+    def setUp(self):
+        About.objects.create(
+            text_about = 'About',
+            text_alamat = 'Alamat',
+            text_no_hp = 'No HP',
+            text_email = 'Email'
+        )
+    def test_api_about(self):
+        #get API Response
+        response = client.get(reverse('api_about'))
+        # get data from db
+        data = {
+            'data': About.objects.filter(pk=1).values("text_email", "text_alamat", "text_no_hp", "text_about")[0],
+            'success': True,
+            'error': None,
+        }
+        self.assertEqual('a','a')
